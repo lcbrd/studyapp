0e7bf5e7e58c18f1ee9a65722496467c9ccfcabe
c l <lcbrd@163.com>
2018-11-30 07:51:35 +0800

    dw

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class User(models.Model):
+    user_name = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
+    
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from main.models import User
 # Create your views here.
 def loginhtml(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        u = User.objects.get(user_name=request.POST.
+        print(u.password)
+    return render(request,'login.html',{'title':123})
